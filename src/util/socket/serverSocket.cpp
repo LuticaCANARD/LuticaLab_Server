5b6a45ee6831912d7e50e87c650bf2d1901e34ff
LuticaCANARD <presan100@gmail.com>
2024-04-17 15:28:16 +0900

    fix : 총론및 원칙 수정/소켓 크로스플랫폼 준비

---
+++ src/util/socket/serverSocket.cpp
@@ -0,0 +1,6 @@
+#if !defined(_WIN32) && (defined(__unix__) || defined(__unix) || (defined(__APPLE__) && defined(__MACH__)))
+    /* UNIX-style OS. ------------------------------------------- */
+
+#else
+
+#endif
