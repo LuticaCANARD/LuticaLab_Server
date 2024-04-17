2811f373c4a7afcf472324d2c1a0be58c71948fa
Lutica CANARD <presan100@gmail.com>
2024-04-18 03:46:27 +0900

    aa

--- src/util/socket/serverSocket.cpp
+++
@@ -1,6 +0,0 @@
-#if !defined(_WIN32) && (defined(__unix__) || defined(__unix) || (defined(__APPLE__) && defined(__MACH__)))
-    /* UNIX-style OS. ------------------------------------------- */
-
-#else
-
-#endif
