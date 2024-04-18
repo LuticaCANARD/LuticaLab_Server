beab8f0f4eedbbf626959c10b2658dc86d413ca1
LuticaCANARD <presan100@gmail.com>
2024-04-18 16:56:18 +0900

    feat : 예제 tcp stream만듦.

--- src/util/socket/socket_manager.rs
+++ src/util/socket/socket_manager.rs
@@ -1,11 +1,34 @@
-use std::net::*;
 pub mod socket_manager 
 {
-    // Socket들의 연결을 관리하고자 하는 객체.
+    use std::net::*;
+    use std::collections::HashMap;
+    // Socket들의 연결을 관리하고자 하는 모듈.
+    pub trait ConnectManager {
+        fn regist (&mut self,target:TcpStream);
+    }
+    
+    pub struct UserConnectionTCP
+    {
+        connection_id:i32,
+        connect_socket:TcpStream
+    }
+
+    pub struct UserConnectionTCPManager
+    {
+        connections : HashMap<i32,UserConnectionTCP>,
+        last_id : i32
+    }
+
+    impl ConnectManager for UserConnectionTCPManager {
+        fn regist (&mut self,target:TcpStream ) {
+            let conn = UserConnectionTCP
+            {
+                connection_id : self.last_id,
+                connect_socket : target
+            };
+            self.connections.insert(self.last_id, conn);
+            self.last_id += 1;
+        }
+    }
 }
 
-struct UserConnection
-{
-    connection_id:i32,
-    connect_socket:TcpListener
-}
