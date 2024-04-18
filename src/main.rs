0ae72fe5227b0759de9def0945c2e3f1a5c2363f
LuticaCANARD <presan100@gmail.com>
2024-04-18 18:57:14 +0900

    feat : request 취득

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod util;
+use std::borrow::BorrowMut;
 use std::net::*;
 use std::io::prelude::*;
 
@@ -6,16 +7,27 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
 
+        let stream = stream.unwrap();
         println!("Connection established!");
         handle_connection(stream);
     }
 }
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
-
     stream.read(&mut buffer).unwrap();
 
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let contents = String::from("RESPONSE");
+    let req = String::from_utf8_lossy(&buffer);
+    println!("{:?}",req);
+
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
+    );
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+
 }
